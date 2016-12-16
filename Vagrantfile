9d610bacfa9e1d1a6d773b41ae5a9a6f4d5f5a7c
Michael Alicea <mfalicea58@gmail.com>
2016-12-15 21:20:06 -0500

    Broke everything lolz

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,29 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
-Vagrant.configure('2') do |config|
-  config.vm.box      = 'ubuntu/wily64'
-  config.vm.hostname = 'dev-box'
 
-  config.vm.network :forwarded_port, guest: 8080, host: 8080
+# All Vagrant configuration is done below. The "2" in Vagrant.configure
+# configures the configuration version (we support older styles for
+# backwards compatibility). Please don't change it unless you know what
+# you're doing.
+Vagrant.configure("2") do |config|
+  config.vm.box = "ubuntu/xenial64"
+
+  config.vm.network :forwarded_port, guest: 8081, host: 8081 
+  config.vm.network :forwarded_port, guest: 27017, host: 27017 
+
+  config.vm.synced_folder '.', '/vagrant'
 
   config.vm.provision :shell, path: 'bootstrap.sh', keep_color: true
 
-  config.vm.provider 'virtualbox' do |v|
-    v.memory = 1024
-    v.cpus = 2
+  config.vm.provider "virtualbox" do |v|
+    v.name = "MEAN16-BankinIt"
+    # max 75% CPU cap
+    v.customize ["modifyvm", :id, "--cpuexecutioncap", "75"]
+    # 2GB ram
+    v.memory = 2048
+    v.customize ["setextradata", :id, 
+                  "VBoxInternal2/SharedFoldersEnableSymlinksCreate/vagrant","1"]
+
   end
+
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,63 +1,38 @@
-# The output of all these installation steps is noisy. With this utility
-# the progress report is nice and concise.
-function install {
-    echo installing $1
-    shift
-    apt-get -y install "$@" >/dev/null 2>&1
-}
+#!/usr/bin/env bash
 
-echo adding swap file
-fallocate -l 2G /swapfile
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
-echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+echo "Provision VM START"
+echo "=========================================="
 
-echo updating package information
-apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
-apt-get -y update >/dev/null 2>&1
+sudo apt-get update
 
-install 'development tools' build-essential
+#install curl
+sudo apt-get -y install curl 
 
-install Ruby ruby2.3 ruby2.3-dev
-update-alternatives --set ruby /usr/bin/ruby2.3 >/dev/null 2>&1
-update-alternatives --set gem /usr/bin/gem2.3 >/dev/null 2>&1
+#install nodjs
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_7.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo apt-get update
+sudo apt-get -y install nodejs
+sudo apt-get -y install build-essential
 
-echo installing Bundler
-gem install bundler -N >/dev/null 2>&1
+#install mongo db
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get update
+sudo sed -i '/bind_ip = 127.0.0.1/,/bind_ip\ =\ 127\.0\.0\.1/s/^/#/' /etc/mongod.conf
+sudo service mongod restart
 
-install Git git
-install SQLite sqlite3 libsqlite3-dev
-install memcached memcached
-install Redis redis-server
-install RabbitMQ rabbitmq-server
 
-install PostgreSQL postgresql postgresql-contrib libpq-dev
-sudo -u postgres createuser --superuser vagrant
-sudo -u postgres createdb -O vagrant activerecord_unittest
-sudo -u postgres createdb -O vagrant activerecord_unittest2
+#install base npm packages
+sudo npm install -g bower
+sudo npm install -g grunt-cli
+sudo npm install -g express
 
-echo installing Node and NPM
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-install MySQL mysql-server libmysqlclient-dev
-mysql -uroot -proot <<SQL
-CREATE USER 'rails'@'localhost';
-CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
-SQL
-
-install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
-install 'Blade dependencies' libncurses5-dev
-install 'ExecJS runtime' nodejs
-
-# Needed for docs generation.
-update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
-
-echo 'all set, rock on!'
+echo ""
+echo "=========================================="
+echo "Node setup:"
+node -v
+echo "Provision VM finished"
