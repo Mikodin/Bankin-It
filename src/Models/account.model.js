b59423f1e783479aa1c2d25faa15fa409548f0b7
Michael Alicea <mfalicea58@gmail.com>
2017-04-19 20:24:27 -0400

    Add the account model

---
+++ src/Models/account.model.js
@@ -0,0 +1,14 @@
+export default class Account {
+  constructor(name, initialAmount, percentage) {
+    // TODO: Hacky but it works for now.  I'll pull in a library at some point
+    this.id = Math.floor(Math.random() * 100000);
+    this.name = name;
+    this.percentageOfParent = percentage;
+    this.amount = this.calculateAmount(initialAmount, this.percentageOfParent);
+    this.percent = 100;
+  }
+
+  calculateAmount(amount, percentage) {
+    return amount * (percentage / 100);
+  }
+}
