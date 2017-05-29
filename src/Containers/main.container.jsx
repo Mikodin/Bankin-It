03087525232d7525d44f7b3631707414758736bb
Michael Alicea <mfalicea58@gmail.com>
2017-05-29 18:10:50 -0400

    Added a dimmer for when data is loading

--- src/Containers/main.container.jsx
+++ src/Containers/main.container.jsx
@@ -2,7 +2,14 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { connect } from 'react-redux';
-import { Container, Header, Icon } from 'semantic-ui-react';
+import {
+  Container,
+  Header,
+  Icon,
+  Dimmer,
+  Loader,
+  Segment 
+} from 'semantic-ui-react';
 
 import HeaderContainer from './header.container';
 import MonthliesContainer from './monthlies.container';
@@ -27,36 +34,17 @@ class MainContainer extends Component {
   }
 
   render() {
-    const header =
-      (
-        <Header as="h1" icon>
-          <Icon name="money" />
-          Bankin-It
-          <Header.Subheader>
-            The sum is more than just the parts of the whole
-          </Header.Subheader>
-          <Header.Subheader>
-            Or some other cheesy quote
-          </Header.Subheader>
-        </Header>
-      );
-
-    const body =
-      this.props.fetchingData
-        ?
-        <h1> Loading...</h1>
-        :
-        (<div>
-            <MonthliesContainer />
-            <AccountsContainer />
-          </div>);
-
     return (
       <div>
         <HeaderContainer />
         <Container style={{ paddingTop: '50px' }}>
-          {header}
-          { body }
+          <Dimmer.Dimmable as={Segment} dimmed={this.props.fetchingData}>
+            <Dimmer active={this.props.fetchingData} inverted>
+              <Loader>Loading</Loader>
+            </Dimmer>
+            <MonthliesContainer />
+            <AccountsContainer />
+          </Dimmer.Dimmable>
         </Container>
       </div>
     );
